67c117da4ca9c17a166e979333e7b6dfed2afd9a
OopsItsPB <127781020+prabhathreddybhuma@users.noreply.github.com>
2025-04-13 16:26:47 +0530

    port 5050

--- backend/app.js
+++ backend/app.js
@@ -36,5 +36,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: 'Internal Server Error' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
